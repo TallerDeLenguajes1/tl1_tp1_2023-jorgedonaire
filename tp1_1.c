c98a7ffda0bc0e48c3978513bb17b6b5cf15a6c7
Jorge Donaire <jorgedonaire93@gmail.com>
2023-03-23 01:30:20 -0300

    Desarrollo del apartado e)

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola Mundo");
+    int *punt;
+    int puntero=5;
+
+    punt = &puntero;
+
+    printf("El contenido del puntero es: %d \n", *punt);
+    printf("La direccion de memoria almacenada por el puntero: %d \n", punt);
+    printf("La direccion de memoria de la variable: %d \n", &puntero);
+    printf("La direccion de memoria del puntero: %d \n", &punt);
+    printf("El tamaño de memoria utilizado por la variable: %d \n", sizeof(*punt));
+
+
     return 0;
 }
